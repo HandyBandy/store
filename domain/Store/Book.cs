96165f5c89a5832235c9dc19a5e6583cd9f828d7
Илья Антюхов <b0tec@outlook.com>
2023-06-29 22:51:16 +0300

    Добавили в класс Book описание

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,6 +1,15 @@
-﻿namespace Store;
+﻿using System;
 
-public class Class1
+namespace Store;
+
+public class Book
 {
+    public string Title { get; }  
 
+    public int Id { get; }
+    public Book(int id, string title)
+    {
+        Id = Id;
+        Title = title;
+    }
 }
